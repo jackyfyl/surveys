8e28711aa9a985d6bff0a4c7247e7c8ea59d8121
jacky <yinliang.feng@outlook.com>
2016-01-14 16:26:00 +0800

    Impl add/edit with bingo.

--- app/Controller/SurveysController.php
+++ app/Controller/SurveysController.php
@@ -17,7 +17,7 @@ class SurveysController extends AppController {
 			$this->set('_serialize', array_keys($this->viewVars));
 		}
 	}
-	
+
 	public function beforeFilter() {
 		header("Access-Control-Allow-Origin: *");
 	}
@@ -116,23 +116,71 @@ class SurveysController extends AppController {
 		return $this->redirect(array('action' => 'index'));
 	}
 
-	public function add2() {
+	public function addwithbingo() {
 		if ($this->request->is('post')) {
 			// check survey_validation
-			$column_name = 'q03';
-			$surveyname = $this->request->data['Survey']['q01'];
-			$phone = $this->request->data['Survey'][$column_name];
+			$user_name_column = 'q01';
+			$survey_name_column = 'userid';
+			$surveyname = $this->request->data['Survey'][$survey_name_column];
+			$user_name = $this->request->data['Survey'][$user_name_column];
 
 			$this->loadModel("SurveyValidation");
-			echo $this->SurveyValidation->exists_one($surveyname, $column_name, $phone);
+			$valid_user = $this->SurveyValidation->exists_one($surveyname, $user_name_column, $user_name);
+			if (!$valid_user) {
+				if ($this->isJsonOrXmlExt()){
+					$this->set('error', 'user is invalid.');
+					$this->set('_serialize', array_keys($this->viewVars));
+					return;
+				} else {
+					$this->Session->setFlash(__('user is invalid.'));
+				}
+			}
 
-			return;
-			
+			$conditions = array($survey_name_column => $surveyname, $user_name_column => $user_name);
+			$this->set($survey_name_column, $surveyname);
+			$this->set($user_name_column, $user_name);
+			$count = $this->Survey->find('count', array('conditions' => $conditions));
+			if ($count > 0) {
+				if ($this->isJsonOrXmlExt()){
+					$this->set('error', 'user exists.');
+					$this->set('_serialize', array_keys($this->viewVars));
+					return;
+				} else {
+					$this->Session->setFlash(__('user exists.'));
+				}
+			}
+
 			$this->Survey->create();
 			if ($this->Survey->save($this->request->data)) {
-				// check if finished
 
-				// 
+				// check if finished
+				$finished_name = 'q48';
+				if (isset($this->request->data['Survey'][$finished_name])) {
+					$finished = $this->request->data['Survey'][$finished_name];
+				}
+				else
+				{
+					$finished = false;
+				}
+
+				$this->set('finished', $finished);
+				//
+				if ($finished == 1) {
+					$id = $this->Survey->id;
+					$conditions = array('id <=' => $id);
+					$order = $this->Survey->find('count', array('conditions' => $conditions));
+					$this->set('order', $order);
+
+					$this->loadModel('BingoNumber');
+					$count = $this->BingoNumber->find('count', array('conditions' => array('bingo_number' => $order) ));
+					if ($count > 0) {
+						// Bingo！
+						$bingo_column = 'q50';
+						$this->Survey->set($bingo_column, 1);
+						$this->Survey->save();
+						$this->set('bingo', 1);
+					}
+				}
 
 
 				if ($this->isJsonOrXmlExt()){
@@ -141,7 +189,7 @@ class SurveysController extends AppController {
 					return;
 				} else {
 					$this->Session->setFlash(__('The survey has been saved.'));
-					return $this->redirect(array('action' => 'index'));
+					//return $this->redirect(array('action' => 'index'));
 				}
 			} else {
 				if ($this->isJsonOrXmlExt()){
@@ -155,4 +203,99 @@ class SurveysController extends AppController {
 			}
 		}
 	}
+
+	public function editwithbingo($id = null) {
+		if (!$this->Survey->exists($id)) {
+			throw new NotFoundException(__('Invalid survey'));
+		}
+
+		$this->request->data[$this->modelClass]['id'] = $id;
+		if ($this->request->is(array('post', 'put'))) {
+			// check survey_validation
+			$user_name_column = 'q01';
+			$survey_name_column = 'userid';
+			$surveyname = $this->request->data['Survey'][$survey_name_column];
+			$user_name = $this->request->data['Survey'][$user_name_column];
+
+			$this->loadModel("SurveyValidation");
+			$valid_user = $this->SurveyValidation->exists_one($surveyname, $user_name_column, $user_name);
+			if (!$valid_user) {
+				if ($this->isJsonOrXmlExt()){
+					$this->set('error', 'user is invalid.');
+					$this->set('_serialize', array_keys($this->viewVars));
+					return;
+				} else {
+					$this->Session->setFlash(__('user is invalid.'));
+				}
+			}
+
+			$conditions = array($survey_name_column => $surveyname, $user_name_column => $user_name, 'id <>' => $id);
+			$this->set($survey_name_column, $surveyname);
+			$this->set($user_name_column, $user_name);
+			$count = $this->Survey->find('count', array('conditions' => $conditions));
+			if ($count > 0) {
+				if ($this->isJsonOrXmlExt()){
+					$this->set('error', 'user exists.');
+					$this->set('_serialize', array_keys($this->viewVars));
+					return;
+				} else {
+					$this->Session->setFlash(__('user exists.'));
+				}
+			}
+
+			if ($this->Survey->save($this->request->data)) {
+
+				// check if finished
+				$finished_name = 'q48';
+				if (isset($this->request->data['Survey'][$finished_name])) {
+					$finished = $this->request->data['Survey'][$finished_name];
+				}
+				else
+				{
+					$finished = false;
+				}
+
+				$this->set('finished', $finished);
+				//
+				if ($finished == 1) {
+					$id = $this->Survey->id;
+					$conditions = array('id <=' => $id);
+					$order = $this->Survey->find('count', array('conditions' => $conditions));
+					$this->set('order', $order);
+
+					$this->loadModel('BingoNumber');
+					$count = $this->BingoNumber->find('count', array('conditions' => array('bingo_number' => $order) ));
+					if ($count > 0) {
+						// Bingo！
+						$bingo_column = 'q50';
+						$this->Survey->set($bingo_column, 1);
+						$this->Survey->save();
+						$this->set('bingo', 1);
+					}
+				}
+
+
+				if ($this->isJsonOrXmlExt()){
+					$this->set('id', $this->Survey->id);
+					$this->set('_serialize', array_keys($this->viewVars));
+					return;
+				} else {
+					$this->Session->setFlash(__('The survey has been saved.'));
+					//return $this->redirect(array('action' => 'index'));
+				}
+			} else {
+				if ($this->isJsonOrXmlExt()){
+					$this->set('error', 'The survey could not be saved. Please, try again.');
+					$this->set('_serialize', array_keys($this->viewVars));
+					return;
+				}
+				else {
+					$this->Session->setFlash(__('The survey could not be saved. Please, try again.'));
+				}
+			}
+		}
+	}
+
+
+
 }
